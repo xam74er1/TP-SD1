e6e32e0256ca70779d2d1cc3e01aa21d6ae090c0
xam74 <xam74er1@gmail.com>
2021-12-19 19:01:01 +0100

    Ajout des raabit

---
+++ rabbit/src/test/java/eu/telecomnancy/rabbit/RabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
